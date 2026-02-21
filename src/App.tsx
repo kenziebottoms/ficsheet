1a52537ed2fe38ee5a6c8ef7ae255da74ae5fde2
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-02-21 01:41:52 -0600

    condense css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
-
 import DailyProjectWordCountForm from './forms/DailyProjectWordCountForm'
 
 function App() {
   return (
     <div className='w-full h-full overflow-y-auto overflow-x-hidden flex flex-col max-w-3xl mx-auto'>
-      <h1 className='text-md'>ficsheet</h1>
+      <h1 className='text-md py-3'>
+        fic<span className='text-secondary'>sheet</span>
+      </h1>
 
       <DailyProjectWordCountForm />
     </div>
