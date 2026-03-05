873af21dc1ad51278feac9f9245482cca5f763e8
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-04 22:12:40 -0600

    fix word count

--- src/forms/DailyProjectWordCountForm.tsx
+++ src/forms/DailyProjectWordCountForm.tsx
@@ -27,7 +27,7 @@ const DailyProjectWordCountForm = () => {
     // TODO: store data
     console.log({
       ...formData,
-      wordCount: pastedWords.trim() === '' ? 0 : pastedWords.trim().split(' ').length,
+      count: pastedWords.trim() === '' ? 0 : pastedWords.trim().split(' ').length,
     } as DailyWordCountEntry);
   }
 
