7e2ecb9c9b26736c5cc77439478bb0b68c4b6638
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-06 00:05:26 -0600

    text shadows on buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,9 +11,9 @@ type Props = PropsWithChildren & {
 }
 
 const ButtonClassNames: Record<ButtonStyle, string> = {
-  primary: 'bg-linear-45/shorter from-indigo-600 via-orange-400 to-indigo-500 from-15% via-80% to-90%',
-  secondary: 'bg-linear-45/oklab from-pink-500 via-pink-400 to-pink-700 from-30% via-80% to-90%',
-  subtle: 'bg-linear-45/shorter from-zinc-700 via-zinc-400/50 to-zinc-700 from-15% via-75% to-85%',
+  primary: 'text-shadow-lg/50 text-shadow-violet-800 bg-linear-45/shorter from-indigo-600 via-orange-400 to-indigo-500 from-15% via-75% to-90%',
+  secondary: 'text-shadow-lg/50 text-shadow-pink-950 bg-linear-45/oklab from-pink-700 via-pink-400 to-pink-700 from-30% via-80% to-90%',
+  subtle: 'text-shadow-lg/30 bg-linear-45/shorter from-zinc-600 via-zinc-400 to-zinc-600 from-20% via-80% to-90%',
 }
 
 const Button = ({
