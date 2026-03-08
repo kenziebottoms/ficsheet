fd99279dc1339cbca3927a0b345345a2459041c8
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-07 21:58:41 -0600

    pink title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     // For rendering MUI charts in dark mode
     <ThemeProvider theme={createTheme({ palette: { mode: 'dark' } })}>
       <h1 className='p-2 text-center border-b-2 border-dashed border-zinc-500'>
-        fic<span className='text-secondary'>sheet</span>
+        fic<span className='text-primary'>sheet</span>
       </h1>
       <div className='w-full h-full flex flex-col lg:flex-row lg:flex-wrap divide-y-2 lg:divide-y-0 lg:divide-x-2 lg divide-zinc-500 divide-dashed'>
         <Charts className='grow' />
