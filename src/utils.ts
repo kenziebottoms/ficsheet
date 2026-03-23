260c97b3f0aeb24054442076cb058a05715b7026
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-22 21:02:29 -0500

    fix and doc getDates beween

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,20 @@
-import { addDays, format } from "date-fns";
+import { addDays, format, isBefore, isSameDay } from "date-fns";
 
 import type { MonthName } from "./types";
 
 export const getMonthName = (date: Date) =>
   date.toLocaleString("default", { month: "long" }) as MonthName;
 
-export const getDatesBetween = (min: Date, max: Date) => {
+/**
+ * @param min The first date
+ * @param max The last date
+ * @returns A daily array of date strings in the format `yyyy-MM-dd`
+ * starting with `min` and ending with `max`
+ */
+export const getDatesBetween = (min: Date, max: Date): string[] => {
   const dates = [];
   let iterDate = new Date(min.getTime());
-  while (iterDate.getTime() < max.getTime()) {
+  while (isBefore(iterDate, max) || isSameDay(iterDate, max)) {
     dates.push(format(iterDate, "yyyy-MM-dd"));
     iterDate = addDays(iterDate, 1);
   }
