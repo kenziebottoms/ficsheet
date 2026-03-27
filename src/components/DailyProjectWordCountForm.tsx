1c293f8005779fe29c0d922951d91a57c153e199
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-26 21:44:22 -0500

    toggle for number input

--- src/components/DailyProjectWordCountForm.tsx
+++ src/components/DailyProjectWordCountForm.tsx
@@ -1,4 +1,6 @@
-import type { SubmitEventHandler } from 'react';
+import { useState, type SubmitEventHandler } from 'react';
+import Switch from '@mui/material/Switch';
+import { addDays, isBefore } from 'date-fns';
 
 import type { WordCountEntry } from '../types';
 
@@ -7,13 +9,13 @@ import DateInput from './DateInput';
 import Dropdown from './Dropdown';
 import Input from './Input';
 import TextArea from './TextArea';
-import { addDays, isBefore } from 'date-fns';
 
 export type DailyProjectWordCountFormValues = {
   date: string;
   fic: string;
   fandom: string;
-  pastedWords: string;
+  pastedWords?: string;
+  count?: string;
 }
 type Props = {
   className?: string;
@@ -21,20 +23,28 @@ type Props = {
 const DailyProjectWordCountForm = ({
   className = ''
 }: Props) => {
+  const [showTextarea, setShowTextarea] = useState<boolean>(true)
+
   const handleSubmit: SubmitEventHandler<HTMLFormElement> = e => {
     // Prevent the browser from reloading the page
     e.preventDefault();
 
-    const {
-      pastedWords,
-      ...formData
-    } = Object.fromEntries(new FormData(e.target).entries()) as DailyProjectWordCountFormValues;
+    const formData = Object.fromEntries(new FormData(e.target).entries()) as DailyProjectWordCountFormValues;
+    const entry: WordCountEntry = {
+      date: formData.date,
+      fandom: formData.fandom,
+      fic: formData.fic,
+      count: parseInt(formData.count || '0', 10) || 0
+    }
+
+    if (entry.count === 0) {
+      const pastedWords = (formData.pastedWords || '').trim();
+      entry.count = pastedWords === '' ? 0 : pastedWords.split(' ').length
+    }
 
     // TODO: store data
-    console.log({
-      ...formData,
-      count: pastedWords.trim() === '' ? 0 : pastedWords.trim().split(' ').length,
-    } as WordCountEntry);
+    console.log('formData', formData);
+    console.log('entry', entry);
   }
 
   return <form
@@ -61,10 +71,24 @@ const DailyProjectWordCountForm = ({
         "Mad Max"
       ]}
     />
-    <TextArea
-      label="Words"
-      name="pastedWords"
-    />
+
+    <label className='flex flex-row gap-2'>
+      Count my words
+      <Switch
+        color="secondary"
+        value={showTextarea}
+        onChange={e => setShowTextarea(e.target.checked)}
+        defaultChecked
+      />
+    </label>
+
+    {showTextarea ?
+      <TextArea
+        label="Words"
+        name="pastedWords"
+      /> :
+      <Input label="Word count" type="number" name="count" />}
+
     <Button
       type="submit"
       style="primary"
