e93be38970bf9a24cfb1c400c190fb1efb633f2e
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-28 22:13:34 -0500

    use daily totals for daily word count stats

--- src/components/Charts/YearlyCharts/MonthlyCharts/DailyWordCountStats.tsx
+++ src/components/Charts/YearlyCharts/MonthlyCharts/DailyWordCountStats.tsx
@@ -12,23 +12,23 @@ import { filterByYearAndMonth } from '../utils'
 import DailyTotalSparkline from './DailyTotalSparkline'
 
 const DailyWordCountStats = () => {
-  const { dailyEntries } = use(DataCacheContext)
+  const { dailyTotals } = use(DataCacheContext)
   const { year } = use(YearContext)
   const { month } = use(MonthContext)
-  const filteredEntries = filterByYearAndMonth(dailyEntries, year, month, true)
+  const filteredTotals = filterByYearAndMonth(dailyTotals, year, month, true)
 
   return (
     <div className='bg-zinc-900 p-3 rounded-md space-y-3 w-auto flex flex-col items-start'>
       <DailyTotalSparkline />
       <div className='flex flex-row flex-wrap gap-3'>
         <Badge title="Maximum" style="primary">
-          <span className='font-semibold text-white'>{_.maxBy(filteredEntries, 'count')?.count || 0}</span> words
+          <span className='font-semibold text-white'>{_.maxBy(filteredTotals, 'daily_total')?.daily_total || 0}</span> words
         </Badge>
         <Badge title="Average" style="secondary">
-          <span className='font-semibold text-white'>{_.meanBy(filteredEntries, 'count').toFixed(1)}</span> words
+          <span className='font-semibold text-white'>{_.meanBy(filteredTotals, 'daily_total').toFixed(1)}</span> words
         </Badge>
-        <Badge title="Minimum" style="subtle">
-          <span className='font-semibold text-white'>{_.minBy(filteredEntries, 'count')?.count || 0}</span> words
+        <Badge title="Minimum (excl. 0)" style="subtle">
+          <span className='font-semibold text-white'>{_.min(_.without(_.map(filteredTotals, 'daily_total'), 0)) || 0}</span> words
         </Badge>
       </div>
     </div>
