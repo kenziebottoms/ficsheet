af7eb09c303e0a68374f441d9c524332830c2cd6
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-28 23:18:16 -0500

    longest writing streak

--- src/components/Charts/YearlyCharts/MonthlyCharts/index.tsx
+++ src/components/Charts/YearlyCharts/MonthlyCharts/index.tsx
@@ -9,7 +9,7 @@ import { DataCacheContext } from "@/contexts/DataCache/DataCacheContext";
 import { MonthContext } from "@/contexts/Month/MonthContext";
 import { YearContext } from "@/contexts/Year/YearContext";
 
-import { filterByYearAndMonth } from "../utils";
+import { filterByYearAndMonth, getLongestStreak } from "../utils";
 
 import DailyWordCountStats from "./DailyWordCountStats";
 import DayOfWeekRadar from "./DayOfWeekRadar";
@@ -34,12 +34,15 @@ const MonthlyCharts = () => {
       Edit
     </Button>
     {editMode ? <History /> : <>
-      <div className="flex flex-col gap-3 items-start">
+      <div className="flex flex-row flex-wrap gap-3 items-start">
         <Badge title="Total" style="primary">
           <span className='font-semibold text-white'>{_.sumBy(totals, 'daily_total')}</span> words
         </Badge>
-        <DailyWordCountStats />
+        <Badge title="Longest Streak" style="secondary">
+          <span className='font-semibold text-white'>{getLongestStreak(totals, x => x.daily_total !== 0)}</span> days
+        </Badge>
       </div>
+      <DailyWordCountStats />
       <div className="flex flex-row flex-wrap gap-3">
         <FandomPie />
         <RunningTotalLine />
