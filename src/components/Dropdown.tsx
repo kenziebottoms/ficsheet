ee98a29bfe8859bd60eb0f2b0f267c254aaa6249
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-31 00:44:46 -0500

    default value for dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,27 +1,28 @@
-type Props<T extends string> = {
+import { useState } from "react";
+
+type Props<T extends string | number> = {
   name: string;
   label: string;
   placeholder?: string;
   options: T[];
-  value?: T | '';
-  onChange?: (newValue: T) => void;
+  defaultValue?: T | '';
 }
-const Dropdown = <T extends string,>({
+const Dropdown = <T extends string | number,>({
   name,
   label,
   placeholder,
   options,
-  value,
-  onChange,
+  defaultValue,
 }: Props<T>) => {
+  const [value, setValue] = useState<T | null>(defaultValue || null);
 
   return <div className='flex flex-col'>
     <label>
       <div>{label}</div>
       <select
         name={name}
-        value={value}
-        onChange={onChange ? e => onChange(e.target.value as T) : undefined}
+        value={value ?? ''}
+        onChange={e => setValue(e.target.value as T)}
         className='w-full rounded-md p-2 border-2 border-primary/50 focus-within:border-primary outline-0'
       >
         <option value="">{placeholder}</option>
