c50098d53ae4942293696b533c9ba01c934cb7ae
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-03-31 17:17:59 -0500

    even more compact small button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -25,7 +25,7 @@ const Button = ({
     type={type}
     onClick={onClick}
     className={[
-      small ? 'text-md rounded-md py-[0.2rem] px-3 gap-2' : 'text-xl rounded-lg py-1 px-6 gap-3',
+      small ? 'text-md rounded-md py-[0.2rem] px-2 gap-2' : 'text-xl rounded-lg py-1 px-6 gap-3',
       'flex flex-row items-center justify-center font-mono font-medium cursor-pointer transition duration-150 ease-in-out hover:brightness-125 hover:saturate-75 outline-primary-highlight focus-within:outline-primary-highlight',
       className,
       ButtonBackgroundClassNames[style],
