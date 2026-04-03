ea36c3d6e5ba957f475597604e46fb9858b51741
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-04-03 17:54:09 -0500

    widget classname

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,13 +3,15 @@ import type { PropsWithChildren } from "react";
 type Props = PropsWithChildren & {
   title: string;
   subtitle?: string;
+  className?: string;
 }
 const Widget = ({
   title,
   subtitle,
+  className = '',
   children,
 }: Props) => {
-  return <div className='rounded-lg bg-zinc-800 border-2 border-dashed border-zinc-500 p-3'>
+  return <div className={['rounded-lg bg-zinc-800 border-2 border-dashed border-zinc-500 p-3', className].join(" ")}>
     <h4 className='mb-2'>{title}</h4>
     {subtitle && <h5>{subtitle}</h5>}
     {children}
