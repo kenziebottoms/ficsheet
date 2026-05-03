d482c23c74c0f8e1d822da291a71ae7e0202072a
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-05-02 22:50:48 -0500

    more dynamic color palettes

--- src/components/Charts/constants.ts
+++ src/components/Charts/constants.ts
@@ -1,22 +1,22 @@
-export const colors = ["#4f46e5", "#fb923c", "#f6339a", "#953fed"];
-
-export const fullRainbow = [
-  "#f472b6",
-  "#e11d48",
-  "#f97316",
-  "#facc15",
-  "#84cc16",
-  "#059669",
-  "#0ea5e9",
-  "#4f46e5",
-  "#c026d3",
-  "#fff",
-  "#000",
-];
+export const colors = ["#4f46e5", "#fb923c", "#f6339a", "#0ea5e9"];
 
 export const getDynamicColorPalette = (numberOfItems: number) => {
-  if (numberOfItems > 4) {
-    return fullRainbow;
+  if (numberOfItems > 7) {
+    return [
+      "#f472b6",
+      "#e11d48",
+      "#f97316",
+      "#facc15",
+      "#84cc16",
+      "#059669",
+      "#0ea5e9",
+      "#4f46e5",
+      "#c026d3",
+      "#fff",
+      "#000",
+    ];
+  } else if (numberOfItems > 4) {
+    return ["#e11d48", "#f97316", "#facc15", "#22c55e", "#4f46e5", "#c026d3"];
   }
   return colors;
 };
