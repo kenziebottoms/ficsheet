ba8e0e1870eb29bdc44c043c8e525f98955b3158
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-05-22 18:43:46 -0500

    have history scroll horizontally without stretching whole page out

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <div className='text-center border-b-2 border-dashed border-zinc-500 flex flex-row justify-between items-center gap-4 px-4 py-2'>
         <h1 className='grow'>fic<span className='text-primary'>sheet</span></h1>
       </div>
-      <div className='w-full h-full flex flex-col lg:flex-row lg:flex-wrap'>
-        <Charts className='grow' />
+      <div className='w-100vw h-full flex flex-col lg:flex-row lg:flex-wrap'>
+        <Charts />
       </div>
     </ThemeProvider>
   )
