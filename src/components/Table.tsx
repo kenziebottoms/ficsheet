eead1aee4144255ce97e46c3834a608834dfab93
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-05-22 19:09:29 -0500

    round table corners

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,33 +8,37 @@ const Table = ({
   headers,
   data,
 }: Props) => {
-  return <table className="font-mono w-full rounded-t-xl bg-zinc-950">
-    <thead className="font-medium">
+  return <table className="font-mono w-full rounded-xl bg-zinc-950">
+    <thead className="font-medium rounded-t-xl">
       <tr className='rounded-t-xl bg-pink-700/50 from-30% via-80% to-90%'>
         {headers.map((header, headerIndex) => <th
           key={header}
           className={[
             "whitespace-nowrap font-mono text-lg",
+            headerIndex === 0 ? 'rounded-tl-xl' : '',
             (headerIndex % 2 === 0) ? "bg-pink-400/25" : "",
+            headerIndex === headers.length - 1 ? 'rounded-tr-xl' : '',
           ].join(" ")}
         >
           {header}
         </th>)}
       </tr>
     </thead>
-    <tbody className="font-normal">
+    <tbody className="font-normal rounded-b-xl">
       {data.map((row, rowIndex) => <tr
         key={rowIndex}
         className={[
           "border-x border-primary/50",
           (rowIndex + 1) % 4 === 0 ? 'bg-pink-700/20' : '',
           (rowIndex + 3) % 4 === 0 ? 'bg-orange-700/20' : '',
+          rowIndex === data.length - 1 ? 'rounded-b-xl' : '',
         ].join(' ')}
       >
         {row.map((cell, cellIndex) => <td
           key={cellIndex}
           className={[
             (cellIndex % 2 === 0) ? "bg-pink-500/10" : "",
+            cellIndex === 0 ? 'rounded-bl-xl' : '',
           ].join(" ")}
         >
           {cell}
