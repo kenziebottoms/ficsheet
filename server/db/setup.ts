c8deb49e523e5b721f19eec6bfc00f1279d3c8e8
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-06-17 13:51:17 -0500

    drop tables, add ship column

--- server/db/setup.ts
+++ server/db/setup.ts
@@ -5,13 +5,19 @@ export const createTables = () => {
   console.log("creating tables");
   db.exec(`
     CREATE TABLE IF NOT EXISTS word_count (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      date TEXT,
-      count INTEGER,
-      fic TEXT,
-      fandom TEXT
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      date TEXT NOT NULL,
+      count INTEGER NOT NULL,
+      fic TEXT NOT NULL,
+      fandom TEXT NOT NULL,
+      ship TEXT
     ) STRICT;
   `);
+};
+
+export const dropTables = () => {
+  console.log("dropping tables");
+  db.exec(`DROP TABLE IF EXISTS word_count;`);
 };
 
 export const seedTables = () => {
