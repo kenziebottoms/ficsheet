798d20962a3a370af385189b7c6fa0485cbd5e7b
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-06-26 18:10:02 -0500

    combine fandom columns in history

--- src/containers/Charts/YearlyCharts/History/index.tsx
+++ src/containers/Charts/YearlyCharts/History/index.tsx
@@ -1,7 +1,7 @@
 import { use, useState } from "react";
 import _ from 'lodash'
 import { isFuture, lastDayOfMonth, lastDayOfYear } from "date-fns";
-import { Add } from '@mui/icons-material'
+import { Add } from '@mui/icons-material';
 
 import { DataCacheContext } from "@/contexts/DataCache/DataCacheContext";
 import { MonthContext } from "@/contexts/Month/MonthContext";
@@ -32,7 +32,6 @@ const History = ({ showEmpty }: Props) => {
   const dates = getDatesBetween(new Date(year, month ?? 0, 1), (month == null ? lastDayOfYear : lastDayOfMonth)(new Date(year, month ?? 0, 1)))
     .filter(date => !isFuture(date))
   const filteredDates = dates.filter(date => showEmpty || _.filter(filteredEntries, { date }).length > 0)
-  const fandoms = _.uniq(_.map(filteredEntries, 'fandom')).sort()
 
   return <>
     {newEntry != null && <Modal
@@ -56,27 +55,26 @@ const History = ({ showEmpty }: Props) => {
       <Table
         headers={[
           'Date',
-          ...fandoms,
+          'Entries',
           'Total',
         ]}
-        data={filteredDates.map((date) => ([
-          `${parseInt(date.substring(5, 7), 10)}/${parseInt(date.substring(8, 10), 10)}/${date.substring(2, 4)}`,
-          ...fandoms
-            // map fandoms to list of fandom entries
-            .map(fandom => _.filter(filteredEntries, { fandom, date }))
-            .map((entries, fandomIndex) => <div className="p-1 gap-y-1 flex flex-row flex-wrap gap-2">
-              {entries.map((entry) => <EntryButton key={entry.id} entry={entry} />)}
+        data={filteredDates
+          .map((date) => ({ date, entries: _.filter(filteredEntries, { date }) }))
+          .map(({ date, entries }) => [
+            `${parseInt(date.substring(5, 7), 10)}/${parseInt(date.substring(8, 10), 10)}/${date.substring(2, 4)}`,
+            <div className="p-1 gap-y-1 flex flex-row flex-wrap gap-2">
+              {entries.map((entry) => <EntryButton key={entry.id} entry={entry as WordCountEntry & { id: number }} />)}
               <div className="grow" />
               <Button
                 style="transparent"
                 small
-                onClick={() => setNewEntry({ fandom: fandoms[fandomIndex], date })}
+                onClick={() => setNewEntry({ date })}
                 icon={Add}
                 className="self-end"
               />
-            </div>),
-          <span className="font-bold">{_.find(filteredDailyTotals, { date })?.daily_total || 0}</span>
-        ]))}
+            </div>,
+            <span className="font-bold">{_.find(filteredDailyTotals, { date })?.daily_total || 0}</span>
+          ])}
       />
     </div>
   </>
